83d0d0acf577e16fe598878d7d2966d0c8899637
yavor300 <yavor300@gmail.com>
2023-10-21 16:50:13 +0300

    [WMS-3] Add Hibernate and Lombok.

--- wms/src/main/java/module-info.java
+++ wms/src/main/java/module-info.java
@@ -9,6 +9,7 @@ module bg.tuvarna.sit.wms {
   requires org.kordamp.bootstrapfx.core;
   requires org.apache.logging.log4j;
   requires org.hibernate.orm.core;
+  requires lombok;
 
   opens bg.tuvarna.sit.wms to javafx.fxml;
   exports bg.tuvarna.sit.wms;
