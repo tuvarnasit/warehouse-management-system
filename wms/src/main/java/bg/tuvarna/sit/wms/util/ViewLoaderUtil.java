c8f058687bf3f743c9233354290a63e12fdf8225
yavor300 <yavor300@gmail.com>
2023-12-25 15:32:45 +0200

    [WMS-11] Refactor the view loader class to use the controller factory when loading the views.

--- wms/src/main/java/bg/tuvarna/sit/wms/util/ViewLoaderUtil.java
+++ wms/src/main/java/bg/tuvarna/sit/wms/util/ViewLoaderUtil.java
@@ -1,5 +1,7 @@
 package bg.tuvarna.sit.wms.util;
 
+import bg.tuvarna.sit.wms.context.ApplicationContext;
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -7,9 +9,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.stage.Stage;
-import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-import java.io.IOException;
+import org.apache.logging.log4j.Logger;
 
 public class ViewLoaderUtil {
 
@@ -23,27 +24,78 @@ public class ViewLoaderUtil {
    */
   public static void loadView(String fxmlPath, ActionEvent event) {
 
+    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    loadView(fxmlPath, stage);
+  }
+
+  /**
+   * Loads a JavaFX view and sets it to the provided stage.
+   *
+   * @param fxmlPath The path to the FXML file of the view.
+   * @param stage    The stage where the view will be set.
+   */
+  public static void loadView(String fxmlPath, Stage stage) {
+
     try {
-      Parent view = FXMLLoader.load(ViewLoaderUtil.class.getResource(fxmlPath));
-      Scene scene = new Scene(view);
-      Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-      window.setScene(scene);
-      window.show();
-    } catch (IOException e) {
-      LOGGER.error("Failed to load the view: " + fxmlPath, e);
-      showAlert("Error", "Failed to load the view.");
+      Parent view = loadFXML(fxmlPath);
+      setScene(stage, view);
     } catch (Exception e) {
-      LOGGER.error("An unexpected error occurred while loading the view: " + fxmlPath, e);
-      showAlert("Error", "An unexpected error occurred.");
+      handleLoadViewException(e, fxmlPath);
     }
   }
 
-  private static void showAlert(String title, String content) {
+  /**
+   * Shows an alert dialog with the specified type, title, and content.
+   *
+   * @param type    The type of the alert.
+   * @param title   The title of the alert.
+   * @param content The content message to be displayed in the alert.
+   */
+  public static void showAlert(Alert.AlertType type, String title, String content) {
 
-    Alert alert = new Alert(Alert.AlertType.ERROR);
+    Alert alert = new Alert(type);
     alert.setTitle(title);
     alert.setHeaderText(null);
     alert.setContentText(content);
     alert.showAndWait();
+  }
+
+  /**
+   * Loads an FXML file and returns its root node.
+   *
+   * @param fxmlPath The path to the FXML file.
+   * @return The root node of the loaded FXML file.
+   * @throws IOException If the file cannot be loaded.
+   */
+  private static Parent loadFXML(String fxmlPath) throws IOException {
+
+    FXMLLoader loader = new FXMLLoader(ViewLoaderUtil.class.getResource(fxmlPath));
+    loader.setControllerFactory(ApplicationContext.getCONTROLLER_FACTORY());
+    return loader.load();
+  }
+
+  /**
+   * Sets the provided view to the given stage.
+   *
+   * @param stage The stage to set the view on.
+   * @param view  The root node of the JavaFX view to set on the stage.
+   */
+  private static void setScene(Stage stage, Parent view) {
+
+    Scene scene = new Scene(view);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  /**
+   * Handles exceptions that occur during the view loading process.
+   *
+   * @param e        The exception that was thrown.
+   * @param fxmlPath The path of the FXML file that was being loaded.
+   */
+  private static void handleLoadViewException(Exception e, String fxmlPath) {
+
+    LOGGER.error("Failed to load the view: " + fxmlPath, e);
+    showAlert(Alert.AlertType.ERROR, "Error", "Failed to load the view.");
   }
 }
