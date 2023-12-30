70f88d794dcda87b250d2c8b3b09d024b0d7c2d9
yavor300 <yavor300@gmail.com>
2023-12-30 23:11:20 +0200

    [WMS-22] Extend the base menu controller from the application controller.

--- wms/src/main/java/bg/tuvarna/sit/wms/controllers/ApplicationController.java
+++ wms/src/main/java/bg/tuvarna/sit/wms/controllers/ApplicationController.java
@@ -1,27 +1,19 @@
 package bg.tuvarna.sit.wms.controllers;
 
+import bg.tuvarna.sit.wms.controllers.base.BaseMenuController;
 import bg.tuvarna.sit.wms.entities.User;
 import bg.tuvarna.sit.wms.session.UserSession;
-import static bg.tuvarna.sit.wms.util.ViewLoaderUtil.loadView;
 import java.util.ArrayList;
 import java.util.Optional;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import lombok.Getter;
 import lombok.Setter;
 
-public class ApplicationController {
-
-  @Setter
-  @FXML
-  private VBox sideMenu;
+public class ApplicationController extends BaseMenuController {
 
   @Setter
   @Getter
@@ -31,47 +23,14 @@ public class ApplicationController {
   @Setter
   private UserSession userSession = UserSession.getInstance();
 
-  private final ToggleGroup toggleGroup = new ToggleGroup();
-
+  @Override
   @FXML
-  void initialize() {
+  protected void initialize() {
 
-    addMenuButton("Warehouses", Optional.empty(), Optional.empty(), e -> loadView("/views/warehouseControlPanel.fxml", e));
-    addMenuButton("Warehouses", Optional.empty(), Optional.empty(), e -> loadView("/views/warehouseControlPanel.fxml", e));
-    addMenuButton("Warehouses", Optional.empty(), Optional.empty(), e -> loadView("/views/warehouseControlPanel.fxml", e));
-    addMenuButton("Register", Optional.of("registerButton"), Optional.empty(), e -> loadView("/views/registration.fxml", e));
-    addMenuButton("Logout", Optional.of("logoutButton"), Optional.of("button-logout"), this::handleLogoutAction);
-    addMenuLabel("Welcome to the dashboard!", Optional.empty());
+    super.initialize();
+
+    addLabelToPane(createLabel("Welcome to the dashboard!"), welcomeSection);
     updateLoginMessageInfo();
-  }
-
-  /**
-   * Handles the logout action.
-   * <p>
-   * This method is triggered when the user clicks the logout button. It performs the logout
-   * operation by clearing the current user session and then redirects the user to the home view.
-   *
-   * @param event The event that triggered this action, typically the logout button click.
-   */
-  @FXML
-  void handleLogoutAction(ActionEvent event) {
-
-    userSession.logout();
-    loadView("/views/home.fxml", event);
-  }
-
-  private void addMenuButton(String text, Optional<String> id, Optional<String> styleClasses, EventHandler<ActionEvent> action) {
-
-    ToggleButton button = new ToggleButton(text);
-    id.ifPresent(button::setId);
-    button.setToggleGroup(toggleGroup);
-
-    // Set default style class and add any additional styles if provided
-    button.getStyleClass().add("menu-button");
-    styleClasses.ifPresent(classes -> button.getStyleClass().addAll(classes.split(" ")));
-
-    button.setOnAction(action);
-    sideMenu.getChildren().add(button);
   }
 
   /**
@@ -82,18 +41,11 @@ public class ApplicationController {
 
     User currentUser = userSession.getCurrentUser();
     if (currentUser != null) {
-      addMenuLabel("You are logged in as: " + currentUser.getFirstName(),
-              Optional.of("loggedInUser"));
+      addLabelToPane(createLabel("You are logged in as: " + currentUser.getFirstName(),
+              Optional.of("loggedInUser")), welcomeSection);
     } else {
       removeLabelById(welcomeSection, "loggedInUser");
     }
-  }
-
-  private void addMenuLabel(String text, Optional<String> id) {
-
-    Label label = new Label(text);
-    id.ifPresent(label::setId);
-    welcomeSection.getChildren().add(label);
   }
 
   private void removeLabelById(Pane container, String labelId) {
