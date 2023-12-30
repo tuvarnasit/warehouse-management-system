f4bed30e909b432e9d8298f5b9a2f76bd0a483f2
yavor300 <yavor300@gmail.com>
2023-12-30 23:12:45 +0200

    [WMS-22] Add exception if the review fails to persist in the databse.

---
+++ wms/src/main/java/bg/tuvarna/sit/wms/exceptions/ReviewPersistenceException.java
@@ -0,0 +1,10 @@
+package bg.tuvarna.sit.wms.exceptions;
+
+/**
+ * Exception thrown to indicate an error during the persistence operations of Review entities.
+ */
+public class ReviewPersistenceException extends Exception {
+    public ReviewPersistenceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
