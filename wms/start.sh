129cf7b63505185fae10587fb1543936d77e6382
yavor300 <yavor300@gmail.com>
2024-01-08 15:32:12 +0200

    [WMS-23] Fix window sizes. Add csv files for fetching data.

--- wms/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export SSO_CREDENTIALS_FILE=credentials.txt
-export DB_USERNAME=admin
-export DB_CONNECTION_URL=jdbc:mysql://wms.cxciwfojwted.eu-west-1.rds.amazonaws.com/dev
-export DB_PASSWORD='DadK8&5x6RyfHk^x'
-./gradlew run
