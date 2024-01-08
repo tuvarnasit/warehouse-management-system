cf339b34c45ea2be3c0c95275330a7a06df08414
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-01-08 14:16:16 +0200

    [WMS-25] Normalize rental request entity by creating a request details entity

--- wms/src/main/java/bg/tuvarna/sit/wms/entities/WarehouseRentalRequest.java
+++ wms/src/main/java/bg/tuvarna/sit/wms/entities/WarehouseRentalRequest.java
@@ -1,10 +1,15 @@
 package bg.tuvarna.sit.wms.entities;
 
 import bg.tuvarna.sit.wms.entities.base.BaseEntity;
+import bg.tuvarna.sit.wms.enums.RequestStatus;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -26,10 +31,18 @@ import javax.persistence.Table;
 public class WarehouseRentalRequest extends BaseEntity {
 
   @ManyToOne
-  @JoinColumn(name = "warehouse_id", nullable = false, referencedColumnName = "id")
-  private Warehouse warehouse;
-
-  @ManyToOne
   @JoinColumn(name = "agent_id", nullable = false, referencedColumnName = "id")
   private Agent agent;
+
+  @ManyToOne(cascade = CascadeType.MERGE)
+  @JoinColumn(name = "request_details_id", nullable = false, referencedColumnName = "id")
+  private RequestDetails requestDetails;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "status", nullable = false)
+  private RequestStatus status;
+
+  @Column(name = "is_invalid", nullable = false)
+  private boolean isInvalid;
+
 }
